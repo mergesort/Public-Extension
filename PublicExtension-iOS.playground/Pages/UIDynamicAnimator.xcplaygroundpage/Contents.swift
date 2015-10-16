008215e3378a7ab2de923ba3a78ac51d828bacf7
Jasdev Singh <jasdevs@gmail.com>
2015-10-16 14:34:55 -0400

    Adding UIDynamicAnimator page

---
+++ PublicExtension-iOS.playground/Pages/UIDynamicAnimator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Table of Contents](Table%20of%20Contents)
+
+import UIKit
+
+extension UIDynamicAnimator {
+    func addBehaviors(behaviors: [UIDynamicBehavior]) {
+        behaviors.forEach {
+            self.addBehavior($0)
+        }
+    }
+}
