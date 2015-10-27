b1c6cd8d439ff147ce1b2e1b06b17fc96f755943
Jasdev Singh <jasdevs@gmail.com>
2015-10-26 21:52:35 -0400

    Adding random Int in range

--- PublicExtension-iOS.playground/Pages/Int.xcplaygroundpage/Contents.swift
+++ PublicExtension-iOS.playground/Pages/Int.xcplaygroundpage/Contents.swift
@@ -1,9 +1,23 @@
 //: [Table of Contents](Table%20of%20Contents)
 
+import Foundation
+
+// MARK: - Repeat block
+
 extension Int {
     func repeatClosure(closure: () -> Void) {
         (0..<self).forEach { _ in
             closure()
         }
+    }
+}
+
+// MARK: - Randon `Int` in specific bounds
+
+//: From the QuickCheck chapter in [Functional Programming in Swift](https://www.objc.io/books/fpinswift/)!
+
+extension Int {
+    static func random(from from: Int, to: Int) -> Int {
+        return from + (Int(arc4random()) % (to - from))
     }
 }
