4a135ce7a6bf256ba71afe294a729c5768544add
Jasdev Singh <jasdevs@gmail.com>
2015-11-18 14:50:36 -0500

    Removign extra comment

--- PublicExtension-iOS.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ PublicExtension-iOS.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -1,6 +1,5 @@
 //: [Table of Contents](Table%20of%20Contents)
 
-// Merge two dictionaries, right-hand side overrides duplicate keys from left-hand side
 func +<U, V>(var lhs: [U: V], rhs: [U: V]) -> [U: V] {
     for (key, value) in rhs {
         lhs[key] = value
