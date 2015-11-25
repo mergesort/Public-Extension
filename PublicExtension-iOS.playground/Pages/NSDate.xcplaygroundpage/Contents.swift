fed5bb9754b20f76330d4d5795982fcc89482709
Jasdev Singh <jasdevs@gmail.com>
2015-11-25 13:44:45 -0500

    Adding == to NSDate

--- PublicExtension-iOS.playground/Pages/NSDate.xcplaygroundpage/Contents.swift
+++ PublicExtension-iOS.playground/Pages/NSDate.xcplaygroundpage/Contents.swift
@@ -4,6 +4,10 @@ import Foundation
 
 extension NSDate: Comparable {}
 
+public func ==(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs === rhs || lhs.compare(rhs) == .OrderedSame
+}
+
 public func <(lhs: NSDate, rhs: NSDate) -> Bool {
     return lhs.compare(rhs) == .OrderedAscending
 }
