dc4db5acf106a91f74afec808aecb4bd285cc420
Jasdev Singh <jasdevs@gmail.com>
2015-12-04 17:06:15 -0500

    Removing extra `self` references

--- PublicExtension-iOS.playground/Pages/Range.xcplaygroundpage/Contents.swift
+++ PublicExtension-iOS.playground/Pages/Range.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 extension Range {
     func indexPathRange(section: Int = 0) -> [NSIndexPath] {
-        return self.map {
+        return map {
             guard let castedIndex = $0 as? Int else { fatalError("Only Ints allowed for NSIndexPaths!") }
 
             return NSIndexPath(forRow: castedIndex, inSection: section)
