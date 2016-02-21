f98163b49606da2e9f15757791691d4ca572fdf7
Jasdev Singh <jasdevs@gmail.com>
2016-02-21 15:52:08 -0500

    Adding subscripting to NSUserDefaults

--- PublicExtension-iOS.playground/Pages/NSUserDefaults.xcplaygroundpage/Contents.swift
+++ PublicExtension-iOS.playground/Pages/NSUserDefaults.xcplaygroundpage/Contents.swift
@@ -3,6 +3,7 @@
 import Foundation
 
 extension NSUserDefaults {
+    // See note on why this should be used sparingly: https://twitter.com/PublicExtension/status/685562312724492290
     func setValue(value: AnyObject?, forKey key: String, synchronize shouldSynchronize: Bool) -> Bool? {
         setValue(value, forKey: key)
         
@@ -10,4 +11,18 @@ extension NSUserDefaults {
     }
     
     // Repeat for other setters
+}
+
+extension NSUserDefaults {
+    subscript(key: String) -> AnyObject? {
+        get {
+            return objectForKey(key)
+        }
+        
+        set {
+            setObject(newValue, forKey: key)
+        }
+    }
+    
+    // For a generalization of this, see https://github.com/radex/SwiftyUserDefaults
 }
