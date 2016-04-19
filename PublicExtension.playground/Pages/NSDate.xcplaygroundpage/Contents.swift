a39bd8b5076e195c669a62b6600a1a6d9e819c67
Jasdev Singh <jasdevs@gmail.com>
2016-04-18 21:49:06 -0400

    Adding briefTimeAgoInWords to NSDate

--- PublicExtension.playground/Pages/NSDate.xcplaygroundpage/Contents.swift
+++ PublicExtension.playground/Pages/NSDate.xcplaygroundpage/Contents.swift
@@ -11,3 +11,29 @@ public func ==(lhs: NSDate, rhs: NSDate) -> Bool {
 public func <(lhs: NSDate, rhs: NSDate) -> Bool {
     return lhs.compare(rhs) == .OrderedAscending
 }
+
+//: Credit to [@soffes](https://twitter.com/soffes)
+extension NSDate {
+    var briefTimeAgoInWords: String {
+        let components = NSCalendar.currentCalendar().components(
+            [.Second, .Minute, .Hour, .Day, .Year], fromDate: self, toDate: NSDate(), options: [])
+        
+        if components.year > 0 {
+            return "\(components.year)y"
+        }
+        
+        if components.day > 0 {
+            return "\(components.day)d"
+        }
+        
+        if components.hour > 0 {
+            return "\(components.hour)h"
+        }
+        
+        if components.minute > 0 {
+            return "\(components.minute)m"
+        }
+        
+        return "\(components.second)s"
+    }
+}
