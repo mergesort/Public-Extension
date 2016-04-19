907d3be9cffffdca0039535f37d1729ac1fc35a7
Jasdev Singh <jasdevs@gmail.com>
2016-04-18 22:04:19 -0400

    Adding fontWithMonospaceNumbers

---
+++ PublicExtension.playground/Pages/UIFont.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Table of Contents](Table%20of%20Contents)
+
+import UIKit
+
+//: Credit to [@soffes](https://twitter.com/soffes)
+extension UIFont {
+    var fontWithMonospaceNumbers: UIFont {
+        let fontDescriptor = UIFontDescriptor(name: fontName, size: pointSize).fontDescriptorByAddingAttributes([
+            UIFontDescriptorFeatureSettingsAttribute: [
+                [
+                    UIFontFeatureTypeIdentifierKey: kNumberSpacingType,
+                    UIFontFeatureSelectorIdentifierKey: kMonospacedNumbersSelector
+                ]
+            ]
+        ])
+        
+        return UIFont(descriptor: fontDescriptor, size: pointSize)
+    }
+}
