5bed8b7ca882e1865a4424c365be294e0feb74a9
Jasdev Singh <jasdevs@gmail.com>
2016-05-08 14:24:00 -0400

    Adding StoryboardBacked

--- PublicExtension.playground/Pages/Miscellaneous.xcplaygroundpage/Contents.swift
+++ PublicExtension.playground/Pages/Miscellaneous.xcplaygroundpage/Contents.swift
@@ -1,4 +1,6 @@
-//: [Previous](@previous)
+//: [Table of Contents](Table%20of%20Contents)
+
+import UIKit
 
 func apply<T, U>(transform: inout T -> U -> ()) -> T -> U -> T {
     return { a in
@@ -20,4 +22,21 @@ extension SomeProtocol {
 
 func fatalError<T: protocol<CustomStringConvertible, ErrorType>>(error: T) {
     fatalError(error.description)
+}
+
+//: Credit to [@sandofsky](https://twitter.com/sandofsky)
+protocol StoryboardBacked: class {
+    static func newFromStoryboardWithName(name: String?, bundle: NSBundle?) -> Self
+}
+
+extension StoryboardBacked {
+    static func newFromStoryboardWithName(name: String? = nil, bundle: NSBundle? = nil) -> Self {
+        guard
+            let controller = UIStoryboard(name: name ?? String(Self), bundle: bundle).instantiateInitialViewController() as? Self
+        else {
+            fatalError("Misconfigured storyboard!")
+        }
+        
+        return controller
+    }
 }
