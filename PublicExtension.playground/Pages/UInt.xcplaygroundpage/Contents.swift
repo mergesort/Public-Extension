7617c939985a12513f849b49ea826a7efcb328b9
Jasdev Singh <jasdevs@gmail.com>
2016-05-25 16:31:12 -0400

    Implementing Javi's suggestion

--- PublicExtension.playground/Pages/UInt.xcplaygroundpage/Contents.swift
+++ PublicExtension.playground/Pages/UInt.xcplaygroundpage/Contents.swift
@@ -2,23 +2,35 @@
 
 //: Adapted from [@kumo](https://twitter.com/kumo) and [@soffes](https://twitter.com/soffes)
 extension UInt {
+    private static let arabicRomanPairs: [(romanValue: String, arabicValue: UInt)] = [
+        (romanValue: "M", arabicValue: 1000),
+        (romanValue: "CM", arabicValue: 900),
+        (romanValue: "D", arabicValue: 500),
+        (romanValue: "CD", arabicValue: 400),
+        (romanValue: "C", arabicValue: 100),
+        (romanValue: "XC", arabicValue: 90),
+        (romanValue: "L", arabicValue: 50),
+        (romanValue: "XL", arabicValue: 40),
+        (romanValue: "X", arabicValue: 10),
+        (romanValue: "IX", arabicValue: 9),
+        (romanValue: "V", arabicValue: 5),
+        (romanValue: "IV", arabicValue: 4),
+        (romanValue: "I", arabicValue: 1)
+    ]
+    
     var romanNumeralRepresentation: String {
-        let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        let arabicValues: [UInt] = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-
         var romanValue = ""
         var startingValue = self
         
-        for (index, romanChar) in romanValues.enumerate() {
-            let arabicValue = arabicValues[index]
-            let quotient = startingValue / arabicValue
+        UInt.arabicRomanPairs.forEach { pair in
+            let quotient = startingValue / pair.arabicValue
             
             if quotient > 0 {
                 (0..<quotient).forEach { _ in
-                    romanValue += romanChar
+                    romanValue += pair.romanValue
                 }
                 
-                startingValue -= quotient*arabicValue
+                startingValue -= quotient*pair.arabicValue
             }
         }
         
