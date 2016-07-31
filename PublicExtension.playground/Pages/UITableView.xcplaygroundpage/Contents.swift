64cd1a4b3b9a0f2de81791c745de84e6984e8a93
Jasdev Singh <jasdevs@gmail.com>
2016-07-30 22:08:54 -0400

    Adds selectAllRows extension to UITableView

--- PublicExtension.playground/Pages/UITableView.xcplaygroundpage/Contents.swift
+++ PublicExtension.playground/Pages/UITableView.xcplaygroundpage/Contents.swift
@@ -27,3 +27,48 @@ extension UITableView {
         return cell
     }
 }
+
+//: Credit to [@irace](https://twitter.com/irace)
+extension UITableView {
+    func selectAllRows() {
+        indexPaths.forEach(selectRow)
+    }
+    
+    private func selectRow(indexPath: IndexPath) {
+        selectRow(at: indexPath, animated: false, scrollPosition: .none)
+    }
+    
+    private struct IndexPathSequence: Sequence {
+        private let tableView: UITableView
+        
+        func makeIterator() -> AnyIterator<IndexPath> {
+            var (section, row) = (0, 0)
+            
+            return AnyIterator {
+                guard section < self.tableView.numberOfSections else { return nil }
+                
+                let indexPath = IndexPath(row: row, section: section)
+                
+                if row < self.tableView.numberOfRows(inSection: section) - 1 {
+                    row += 1
+                }
+                else {
+                    section += 1
+                    row = 0
+                }
+                
+                return indexPath
+            }
+        }
+    }
+    
+    /**
+     *  A sequence of this table view's index paths
+     *
+     *  Don't save iterators from this sequence because the backing table view's data source could change.
+     *  Only use this for in-line computations (e.g. filtering or fast enumeration).
+     */
+    private var indexPaths: IndexPathSequence {
+        return IndexPathSequence(tableView: self)
+    }
+}
