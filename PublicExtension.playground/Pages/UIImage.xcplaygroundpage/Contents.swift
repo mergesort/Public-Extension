8d8c816c714fc4127e33eec9a4041a5545924bce
Jasdev Singh <jasdevs@gmail.com>
2016-10-26 20:32:26 -0400

    Adds `UIImage.image(fromColor:size:)`

--- PublicExtension.playground/Pages/UIImage.xcplaygroundpage/Contents.swift
+++ PublicExtension.playground/Pages/UIImage.xcplaygroundpage/Contents.swift
@@ -7,3 +7,21 @@ extension UIImage {
         return UIImage(named: "corgi.jpg")!
     }
 }
+
+//: Credit to [@mergesort](https://twitter.com/mergesort)
+extension UIImage {
+    static func image(fromColor color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+        let rect = CGRect(x: 0, y: 0, width: size.height, height: size.height)
+        
+        UIGraphicsBeginImageContext(rect.size)
+        
+        defer { UIGraphicsEndImageContext() }
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
