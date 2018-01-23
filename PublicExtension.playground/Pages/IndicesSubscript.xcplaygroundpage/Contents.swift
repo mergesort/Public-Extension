78803599e85e3b64ab2b11f8add0684e8ad1f35f
Artem Novichkov <novichkoff93@gmail.com>
2018-01-23 21:02:56 +0600

    Move the extension to collection

--- PublicExtension.playground/Pages/IndicesSubscript.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: [Table of Contents](Table%20of%20Contents)
-
-//: Credit to [@iosartem](http://twitter.com/iosartem)
-extension Array {
-
-    subscript(index: [Index]) -> [Element] {
-        return enumerated().filter { index.contains($0.0) }.map { $0.1 }
-    }
-}
