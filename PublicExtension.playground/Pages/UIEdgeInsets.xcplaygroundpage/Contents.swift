09f99d9e55eed539f925bc7371eb05ac88a00b7f
Joe Fabisevich <Joe@fabisevi.ch>
2018-02-15 15:38:44 -0500

    Adding UIEdgeInsets dictionary initializer

--- PublicExtension.playground/Pages/UIEdgeInsets.xcplaygroundpage/Contents.swift
+++ PublicExtension.playground/Pages/UIEdgeInsets.xcplaygroundpage/Contents.swift
@@ -22,3 +22,34 @@ extension UIEdgeInsets {
         right = uniform
     }
 }
+
+//: Credit to [Adam Sharp](https://twitter.com/sharplet) & [Daniel Jalkut](https://twitter.com/danielpunkass)
+extension UIEdgeInsets: ExpressibleByDictionaryLiteral {
+    public typealias Key = EdgeKey
+    public typealias Value = CGFloat
+
+    public enum EdgeKey {
+        case top
+        case left
+        case bottom
+        case right
+    }
+
+    func keyPathForEdge(_ edgeKey: EdgeKey) -> WritableKeyPath<UIEdgeInsets, CGFloat> {
+        switch edgeKey {
+        case .top: return \UIEdgeInsets.top
+        case .left: return \UIEdgeInsets.left
+        case .bottom: return \UIEdgeInsets.bottom
+        case .right: return \UIEdgeInsets.right
+        }
+    }
+
+    public init(dictionaryLiteral elements: (EdgeKey, CGFloat)...) {
+        self = UIEdgeInsets()
+
+        for (inset, value) in elements {
+            let keyPath = self.keyPathForEdge(inset)
+            self[keyPath: keyPath] = value
+        }
+    }
+}
